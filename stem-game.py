aa29aa6cbf76c43a84946a3e8175faf30e599ab3
dweatherwax <dwax413@yahoo.com>
2019-01-13 20:39:34 -0500

    clean up words

--- stem-game.py
+++ stem-game.py
@@ -17,7 +17,7 @@ def draw_background():
     pygame.draw.line(DISPLAYSURF, BLACK, (0, 625), (600, 625), 5)
 
     title_font = pygame.font.SysFont('Arial', 30)
-    title_surface = title_font.render('Mooky Skiing Mystery', False, (0, 0, 0))
+    title_surface = title_font.render('Mooky Skiing Mystery', True, (0, 0, 0))
     DISPLAYSURF.blit(title_surface, (350, 10))
 
     mooky_image = pygame.image.load('mooky.png')
@@ -27,21 +27,21 @@ def draw_background():
 def draw_options():
     
     text_font = pygame.font.SysFont('Arial', 20)
-    text_surface = text_font.render('a. Mooky is awesome', False, (0, 0, 0))
+    text_surface = text_font.render('a. Mooky is awesome', True, (0, 0, 0))
     DISPLAYSURF.blit(text_surface, (10, 635))
 
     text1_font = pygame.font.SysFont('Arial', 20)
-    text1_surface = text1_font.render('b. Mooky is cool', False, (0, 0, 0))
+    text1_surface = text1_font.render('b. Mooky is cool', True, (0, 0, 0))
     DISPLAYSURF.blit(text1_surface, (10, 660))
 
     text2_font = pygame.font.SysFont('Arial', 20)
-    text2_surface = text2_font.render('c. Mooky is the best', False, (0, 0, 0))
+    text2_surface = text2_font.render('c. Mooky is the best', True, (0, 0, 0))
     DISPLAYSURF.blit(text2_surface, (10, 685))
 
 def draw_text():
 
     maintext_font = pygame.font.SysFont('Arial', 20)
-    maintext_surface = maintext_font.render('Mooky is awesome', False, (0, 0, 0))
+    maintext_surface = maintext_font.render('Mooky is awesome', True, (0, 0, 0))
     DISPLAYSURF.blit(maintext_surface, (10, 200))
 
 
