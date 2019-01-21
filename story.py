ed7616c4828e297411ef84dc1b03fe32d5ac39ce
Tim Seiger <tim.seiger+github@gmail.com>
2019-01-21 11:35:06 -0500

    Added text for scene 1

--- story.py
+++ story.py
@@ -1,9 +1,10 @@
 SCENE1 = 'scene1'
 SCENE2 = 'scene2'
+SCENE3 = 'scene3'
 
-scenes = {SCENE1 : ('here is the main story', 
+scenes = {SCENE1 : ("Mooky is bored. Then he has an idea! He will go skiing. He grabs his ski stuff and drives to the nearest ski place, Mooker Woods. He starts to put his ski stuff on. He goes to get his skis, But he can't find them. Oh No! Where are his skis? What should he do?",
                       'mooky.png',  
-                      (('a.', SCENE2), ('b.', SCENE2))), 
+                      (('a. Go search for skis', SCENE2), ('b. Go buy skis', SCENE3))), 
           SCENE2 : ('here is the second scene', 
                       'mooky.png',
                       (('a.', SCENE1), ('b.', SCENE1)) )
